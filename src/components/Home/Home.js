6fbdf08c82daeb9b507b704aa3106f4daafa0b31
MiMNets <mdmonirulislammonir@gmail.com>
2020-10-29 23:11:19 +0600

    first commit of portfolio

---
+++ src/components/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Home.scss'
+const Home = () => {
+    return (
+        <div className="horizontal-scroll-wrapper">
+            <div>Box-1</div>
+            <div>Box-2</div>
+            <div>Box-3</div>
+            <div>Box-4</div>
+            <div>Box-5</div>
+            <div>Box-6</div>
+            <div>Box-7</div>
+            <div>Box-8</div>
+        </div>
+    );
+};
+
+export default Home;
