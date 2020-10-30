a5bd7dd9af9c621601cab8360a0915d7937b0195
MiMNets <mdmonirulislammonir@gmail.com>
2020-10-30 22:22:10 +0600

    link mr-5

---
+++ src/components/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <section className="container">
+            
+        </section>
+    );
+};
+
+export default About;
--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -10,19 +10,19 @@ const TopNav = () => {
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav ml-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link mr-5" href="/">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">About</a>
+        <a class="nav-link mr-5" href="#">About</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Projects</a>
+        <a class="nav-link mr-5" href="#">Projects</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Blog</a>
+        <a class="nav-link mr-5" href="#">Blog</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Contact</a>
+        <a class="nav-link mr-5" href="#">Contact</a>
       </li>
     </ul>
   </div>
