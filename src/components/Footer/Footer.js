e366fb41e43418e1f88848ca08f08c22130be30b
MiMNets <mdmonirulislammonir@gmail.com>
2020-10-30 23:52:20 +0600

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,8 +3,8 @@ import './Footer.css';
 const Footer = () => {
     return (
         
-        <section className="container">
-            <div className="row justify-content-between">
+        <div className="container">
+            <div className="row justify-content-between footer-container">
                 <div className="col-md-10">
                     <span>© 2020 - Monirul Islam,All Right Reserved</span>
                 </div>
@@ -12,7 +12,7 @@ const Footer = () => {
                 <a class="navbar-brand li" href="/"><img src="https://1.bp.blogspot.com/-zTgkeIUvR1o/X5wyYAy5a_I/AAAAAAAAolU/43aLjrNI2jkkU4HdhePcHaHNRfATOUeYACLcBGAsYHQ/s1038/monirul-portfolio.png" alt=""/></a>
                 </div>
             </div>
-        </section>
+        </div>
 
     );
 };
