efde8594047cb0c76d3b965b3958293530ab80ce
MiMNets <mdmonirulislammonir@gmail.com>
2020-10-30 22:16:01 +0600

    main home

--- src/components/MainHome/MainHome.js
+++ src/components/MainHome/MainHome.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import './MainHome.scss';
-
+import './MainHome.css';
 const MainHome = () => {
     return (
-        <div className="horizontal-scroll-wrapper squares">
-            <div>Box-1</div>
-            <div>Box-2</div>
-            <div>Box-3</div>
-            <div>Box-4</div>
-            <div>Box-5</div>
-            <div>Box-6</div>
-            <div>Box-7</div>
-            <div>Box-8</div>
-        </div>
+        <section className="container">
+            <div className="row">
+                <div className="col-md-6 grid">
+                    <h2>WELCOME TO</h2>
+                    <h2><span class="blue-highlight">Monirul Islam Monir's</span> Website!</h2>
+                    <h4>World-class professional Web Developer</h4>
+                    <br/>
+                    <span>I can help you to build your professional portfolio websites, company websites, business websites with using modern web technology; Like : CSS, HTML5, JavaScript, Node.js, etc. I have learn Web Developmet from world's famous web guru <span class="blue-highlight">Jhankar Mahbub</span>. I can also help you to provide technical supprt service regarding your complete IT solutions. I have more than 10 years of experience working as a Technical Support Executive at <a id="icenter-link" href="http://www.icentre-bd.com" target="_blank">iCenter</a>.</span>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <a className="btn btn-primary btn-lg active" role="button" href="https://www.linkedin.com/in/mdmonirulislammonir/" target="_blank">HIRE ME</a>
+                </div>
+                <div className="col-md-6 grid">
+                    <img src="https://1.bp.blogspot.com/-I133jKKCC9g/X5wyeHKUlsI/AAAAAAAAolY/hyzB7NE3Uak03gjqBVsfDifjwr3RetoHgCLcBGAsYHQ/s714/monirul-portfolio2.png" alt="" />
+                </div>
+            </div>
+        </section>
     );
 };
 
