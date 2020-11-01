06d59e46eb8d37f99d600c2ef20fcb40f95a4a8a
MiMNets <mdmonirulislammonir@gmail.com>
2020-11-01 17:09:31 +0600

    projects items

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import MainHome from '../MainHome/MainHome';
+import ProjectItem from '../ProjectItem/ProjectItem';
 const Home = () => {
     return (
         <div>
             <Header></Header>
             <MainHome></MainHome>
+            <ProjectItem></ProjectItem>
             <Footer></Footer>
         </div>
     );
